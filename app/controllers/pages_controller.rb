87dbd3f23b5c339d324527619cc7a87ecae2a580
Nmuta Jones <nmuta@Nmutas-MacBook-Pro.local>
2015-09-06 11:52:27 -0600

    Reinitialize

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+class PagesController < ApplicationController
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+  before_action :authenticate_user!
+
+  def index
+    @message = "welcome"
+  end
+
+end
