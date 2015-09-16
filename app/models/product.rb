1ddb57f52e9d08ccb97fb5fd6f65ff05ca53ef99
Nmuta Jones <nmuta@Nmutas-MacBook-Pro.local>
2015-09-16 10:37:47 -0600

    validate price and change search_for tag

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,10 @@ class Product < ActiveRecord::Base
   belongs_to :order
   belongs_to :user
 
+  before_save :strip_currency_symbol
+  validates_presence_of :name, :description, :price
+  validates_numericality_of :price
+
 
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
@@ -12,5 +16,10 @@ class Product < ActiveRecord::Base
   def hosted_by?(current_user)
     user_id == current_user.id
   end
+
+  private
+    def strip_currency_symbol
+
+    end
 
 end
