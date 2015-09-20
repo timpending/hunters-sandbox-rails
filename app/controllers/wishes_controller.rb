93dbfb34488b155520f8031b4d6d47075eb5b6df
Nmuta Jones <nmuta@Nmutas-MacBook-Pro.local>
2015-09-19 20:15:28 -0600

    auth

--- app/controllers/wishes_controller.rb
+++ app/controllers/wishes_controller.rb
@@ -1,5 +1,6 @@
 class WishesController < ApplicationController
   before_action :set_wish, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   # GET /wishes
   # GET /wishes.json
